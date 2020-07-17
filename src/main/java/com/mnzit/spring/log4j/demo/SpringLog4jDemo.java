f1b073ebbdfe168073e6ffd88cfe2c4815694141
manjit.shakya <manjit.shakya@f1soft.com>
2020-07-17 19:36:12 +0545

    initial

--- src/main/java/com/mnzit/spring/log4j/demo/SpringLog4jDemo.java
+++ src/main/java/com/mnzit/spring/log4j/demo/SpringLog4jDemo.java
@@ -1,5 +1,7 @@
 package com.mnzit.spring.log4j.demo;
 
+import com.mnzit.spring.log4j.demo.test1.Test1;
+import com.mnzit.spring.log4j.demo.test2.Test2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +13,15 @@ public class SpringLog4jDemo {
     public static void main(String[] args) {
         SpringApplication.run(SpringLog4jDemo.class, args);
 
-        log.error("This is an error message for log4j");
-        log.debug("This is an debug message for log4j");
-        log.warn("This is a warning message for log4j");
-        log.info("This is an info message for log4j");
+        while (true) {
+
+            log.error("This is an error message for log4j");
+            log.debug("This is an debug message for log4j");
+            log.warn("This is a warning message for log4j");
+            log.info("This is an info message for log4j");
+
+            new Test1().test();
+            new Test2().test();
+        }
     }
 }
---
+++ src/main/java/com/mnzit/spring/log4j/demo/test2/Test2.java
@@ -0,0 +1,18 @@
+package com.mnzit.spring.log4j.demo.test2;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Manjit Shakya
+ * @email manjit.shakya@f1soft.com
+ */
+@Slf4j
+public class Test2 {
+
+    public void test() {
+        log.error("This is an error message for log4j");
+        log.debug("This is an debug message for log4j");
+        log.warn("This is a warning message for log4j");
+        log.info("This is an info message for log4j");
+    }
+}
